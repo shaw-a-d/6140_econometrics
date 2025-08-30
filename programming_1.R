56fb094bd415b9a9f73a4a0fe93cad5edb838489
shaw-a-d <ashaw93@gatech.edu>
2025-08-29 22:44:59 -0400

    finalized

--- programming_1.R
+++ programming_1.R
@@ -1,5 +1,5 @@
 library(truncnorm) 
-
+rm(list = ls())
 
 set.seed(4289)
 
@@ -10,13 +10,16 @@ x <- c(rtruncnorm(500, a=-Inf, b=Inf, mean=2, sd=0.001),
 
 # step 2
 
-nn <- 0 
+nn <- 100 
 e <- rnorm(nn,0,3)
 
 # step 3
 
 y <- 2*x+e
 
+model <- lm(y~x)
+summary(model)
+coef(model)[2]
 # step 4
 # Summary
 mean(x)
@@ -29,32 +32,38 @@ hist(x, breaks = 18, freq = FALSE, xlim=c(-1,10))
 # Play around with the means and standard deviations to see.
 
 
-nn <- 1000
+nn <- 500
 
 #step 2-Set the number of simulations
-runs<-3
+runs<-100
 
 #step 3-program a function that creates random variables and returns the mean
+# resets betas to empty vector as global vector
+
 bimodal.run <- function(){
-  x <- c(rtruncnorm(nn, a=-Inf, b=Inf, mean=2, sd=10),  rtruncnorm(nn, a=-Inf, b=Inf, mean=8, sd=10))
+  x <<- c(rtruncnorm(nn, a=-Inf, b=Inf, mean=2, sd=10),  rtruncnorm(nn, a=-Inf, b=Inf, mean=8, sd=10))
   e <- rnorm(nn,0,3)
-  y <- 2*x+e
-  data.frame(x = x, y = y)
-  return(mean(x))
+  y <<- 2*x+e
+  #capture beta coefficient per run
+  model <- lm(y~x)
+  betas <- coef(model)[2]
+  mean.x<-mean(x)
+  return(data.frame(mean = mean.x, betas = betas))
+
 }
 
 
-
 #step 4-run simulations from the step 3
-sims <- replicate(runs,bimodal.run()) 
-hist(sims)
+sims <- do.call(rbind, replicate(runs,bimodal.run(), simplify = FALSE))
+print(sims)
 
-hist(data$x)
-
-cor(data$x,data$y)
-plot(data$x,data$y,main="Scatterplot Example",xlab="x", ylab="y",pch=19)
-
-lm(data$y~data$x)
+hist(sims$mean)
 
 
-rtruncnorm
+
+hist(x)
+
+cor(x,y)
+plot(x,y,main="Scatterplot Example",xlab="x", ylab="y",pch=19)
+
+lm(y~x)
