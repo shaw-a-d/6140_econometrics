aa70ad620d77c076e04b9511cb41ee9e0de61719
shaw-a-d <ashaw93@gatech.edu>
2025-09-09 14:56:21 -0400

    9.9 A3 work

--- scripts/A3_code.R
+++ scripts/A3_code.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(tidyverse)
 library(styler)
 
-rm(list = ls())
+
 
 
 load("data/nyts2019.rdata")
@@ -21,3 +21,26 @@ df <- nyts2019
 
 # 01 = Male, 02 = Female, e-cig 01 = Yes, 02 = No
 df <- df[c("StudentLoginID", "Q2", "Q34")]
+
+View(df)
+
+dplyr::count(df, Q34)
+
+df_clean <- df |>
+  filter(Q34 %in% c("01", "02")) |>
+  filter(Q2 %in% c("01", "02"))
+
+View(df_clean)
+rm(df_group)
+
+df_group <- df_clean |>
+  mutate(Q34 = ifelse(Q34 == "02", 0, 1)) |>
+  mutate(Q34 = as.numeric(Q34), Q2 = as.numeric(Q2)) |>
+  rename(sex = Q2, smoke = Q34) |>
+  group_by(sex, smoke)
+
+View(df_group)
+
+t.test(smoke ~ sex, data = df_group, mu = 0, alternative = "two.sided")
+
+# just need to figure out how to do table now
