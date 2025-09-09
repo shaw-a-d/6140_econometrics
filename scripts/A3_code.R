d285e1148c7c3d43e0609c98ea95df0a594d0861
shaw-a-d <ashaw93@gatech.edu>
2025-09-09 13:57:38 -0400

    9.9 A3 work

--- scripts/A3_code.R
+++ scripts/A3_code.R
@@ -1,11 +1,15 @@
-#assignment 3 code
+# assignment 3 code
 # 9-8-25
 
 library(survey)
 library(dplyr)
 library(tidyverse)
+library(styler)
 
-load('data/nyts2019.rdata')
+rm(list = ls())
+
+
+load("data/nyts2019.rdata")
 attach(nyts2019)
 
 
@@ -13,24 +17,7 @@ df <- nyts2019
 
 
 # Create sexXsmoker populations
+# Is there a difference in the proportion of male smokers and female smokers?
 
-# 01 = Male, 02 = Female, e-cig 01 = Yes, 02 = No 
-df_sex <- df[c("StudentLoginID", "Q2", "Q34")]
-
-dplyr::count(df_sex, Q2, Q34)
-
-df_sex <- df_sex %>%
-    filter(!(Q2 == ".N")) |>
-    filter(Q34 == "01")
-
-
-# Create dummy where maleXsmoker = 1, femaleXsmoker = 0
-df_dummy_sex <- df_sex |>
-    mutate(dummy = ifelse(Q2 == "01", 1, 0))
-
-View(df_dummy_sex)
-
-p <- sum(df_dummy_sex$dummy)
-n <- nrow(df_dummy_sex)
-
-binom.test(p, n, p = .5, alternative = "two.sided", conf.level = 0.95)
+# 01 = Male, 02 = Female, e-cig 01 = Yes, 02 = No
+df <- df[c("StudentLoginID", "Q2", "Q34")]
