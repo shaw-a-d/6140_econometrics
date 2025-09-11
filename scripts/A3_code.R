d9dfebb5bb34ffd61486992b61374dfe547cf394
shaw-a-d <ashaw93@gatech.edu>
2025-09-11 09:11:24 -0400

    9.11 work

--- scripts/A3_code.R
+++ scripts/A3_code.R
@@ -16,7 +16,7 @@ attach(nyts2019)
 df <- nyts2019
 
 
-# Create sexXsmoker populations
+
 # Is there a difference in the proportion of male smokers and female smokers?
 
 # 01 = Male, 02 = Female, e-cig 01 = Yes, 02 = No
@@ -28,26 +28,30 @@ df_s <- df |>
   as.numeric(Q37))) |> # nolint
   filter(Q34 == "01" | Q34 == "02") |>
   filter(!(Q34 == "02" & days == 6)) |>
-  filter(days <= 30)
+  filter(days <= 30) |>
+  filter(Q2 %in% c("01", "02"))
 
+dplyr::count(df_s, Q34, Q37)
 
-# df_clean <- df |> # nolint
-#   filter(Q34 %in% c("01", "02")) |> # nolint
-#   filter(Q2 %in% c("01", "02")) # nolint
+df_clean <- df_s |>
+  mutate(current_user = ifelse(days > 0, 1, 0)) |>
+  mutate(Q2 = ifelse(Q2 == "01", "Male", "Female")) |>
+  rename(sex = Q2) |>
+  select(sex, current_user, days)
+
+#Check freq table
 View(df_clean)
+dplyr::count(df_clean, sex, current_user, days)
+nrow(df_clean)
 
-df_group <- # nolint df_clean |>
-  mutate(Q34 = ifelse(Q34 == "02", 0, 1)) |>
-  mutate(Q34 = as.numeric(Q34), Q2 = as.numeric(Q2)) |>
-  rename(sex = Q2, smoke = Q34)
 
-View(df_group)
+results_sex <- t.test(current_user ~ sex, data = df_clean,
+  mu = 0, alternative = "two.sided")
 
-results <- t.test(smoke ~ sex,
-  data = df_group, mu = 0,
-  alternative = "two.sided"
-)
-View(df_group)
-results
+results_sex
 
-languageserver::languageserver_version()
+#Double Check pop proportion means
+sum(df_clean$sex == "Female" & df_clean$current_user == 01)
+sum(df_clean$sex == "Female")
+
+results_sex
