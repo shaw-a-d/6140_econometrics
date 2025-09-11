f57bf6758a9cef6b8be1ae64f6f54fd5f5e9e415
shaw-a-d <ashaw93@gatech.edu>
2025-09-10 22:22:52 -0400

    9.10 clean code

--- scripts/A3_code.R
+++ scripts/A3_code.R
@@ -20,19 +20,23 @@ df <- nyts2019
 # Is there a difference in the proportion of male smokers and female smokers?
 
 # 01 = Male, 02 = Female, e-cig 01 = Yes, 02 = No
-df <- df[c("StudentLoginID", "Q2", "Q34")]
+df <- df[c("StudentLoginID", "Q2", "Q34", "Q37")]
 
-View(df)
+## Cleaned to 18907 students
+df_s <- df |>
+  mutate(days = if_else((Q34 == "02" & Q37 == ".S"), 0,
+  as.numeric(Q37))) |> # nolint
+  filter(Q34 == "01" | Q34 == "02") |>
+  filter(!(Q34 == "02" & days == 6)) |>
+  filter(days <= 30)
 
-dplyr::count(df, Q34)
 
-df_clean <- df |>
-  filter(Q34 %in% c("01", "02")) |>
-  filter(Q2 %in% c("01", "02"))
-
+# df_clean <- df |> # nolint
+#   filter(Q34 %in% c("01", "02")) |> # nolint
+#   filter(Q2 %in% c("01", "02")) # nolint
 View(df_clean)
 
-df_group <- df_clean |>
+df_group <- # nolint df_clean |>
   mutate(Q34 = ifelse(Q34 == "02", 0, 1)) |>
   mutate(Q34 = as.numeric(Q34), Q2 = as.numeric(Q2)) |>
   rename(sex = Q2, smoke = Q34)
@@ -46,5 +50,4 @@ results <- t.test(smoke ~ sex,
 View(df_group)
 results
 
-
-
+languageserver::languageserver_version()
